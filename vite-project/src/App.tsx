67b5a8ef6630a8103cf01365e1790de531c22e4d
JamesMan689 <jameszxu20@gmail.com>
2023-09-09 00:18:57 -0400

    site

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,32 @@
+import {Fragment} from 'react'
+import "./App.css"
+
+function App() {
+
+  return (
+    <Fragment>
+      <div className="home">
+        <div className="title">CHEM  <span style={{backgroundColor: "blue", padding:"5px", borderRadius: "5px", color:"white"}}>TUTOR</span></div>
+      </div>
+
+      <h1 style={{marginTop: "60px"}}>Click a topic to learn more</h1>
+
+      <div className="navigation">
+        <ul>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Classifying Matter</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Electron Configuration and Periodic Table</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Molecular Bonds</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Molecular Orbital Theory</a></li>
+        </ul>
+        <ul>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Thermochemistry</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Stoichiometry</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Net Ionic Equations</a></li>
+          <li><a href="https://www.pornhub.com/view_video.php?viewkey=ph60f6b19a9ac4a" target="_blank" >Gas Laws</a></li>
+        </ul>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
